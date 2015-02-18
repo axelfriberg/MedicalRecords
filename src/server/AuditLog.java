281ff205094917799109e25ab3786a4ec3a187a6
Johan Månsson <mansson.johan@me.com>
2015-02-18 11:43:16 +0100

    AuditLog class created.

---
+++ src/server/AuditLog.java
@@ -0,0 +1,26 @@
+package server;
+
+import java.io.PrintWriter;
+
+/**
+ * Created by johanmansson on 15-02-18.
+ */
+public class AuditLog {
+
+    private PrintWriter writer = new PrintWriter("auditlog.tex", "UTF-8");
+
+    public void writeToLog(String str) {
+
+        writer.println(str);
+
+    }
+
+
+
+
+
+
+
+
+
+}
