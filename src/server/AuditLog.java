c7af62abe7c8db05bb990b14e6f19c147a39e4fa
Johan Månsson <mansson.johan@me.com>
2015-02-20 10:20:29 +0100

    AuditLog class updated.

--- src/server/AuditLog.java
+++ src/server/AuditLog.java
@@ -1,20 +1,39 @@
 package server;
 
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by johanmansson on 15-02-18.
  */
 public class AuditLog {
 
-    private PrintWriter writer = new PrintWriter("auditlog.tex", "UTF-8");
-
-    public void writeToLog(String str) {
-
-        writer.println(str);
-        //to do
+    PrintWriter writer;
 
 
+    try {
+        writer = new PrintWriter("auditlog.tex", "UTF-8");
+    } catch (FileNotFoundException e1) {
+        e1.printStackTrace();
+    } catch (UnsupportedEncodingException e1) {
+        e1.printStackTrace();
+    }
+
+
+
+    public void printConnected(String userType) {
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd-HH.mm").format(Calendar.getInstance().getTime());
+
+        writer.println(timeStamp + " " + userType + " " + "is connected to database");
+
+
+    }
+
+    public void closeLog() {
+        writer.close();
     }
 
 
